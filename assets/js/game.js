86f0bf85efa5ba94e9eb90cd748bd5a57f81af3b
Eric Shumway <ericshumway2@mail.weber.edu>
2022-02-01 22:51:32 -0700

    add enemy combantants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,14 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto"
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var playerMoney = 10;
 
-var fight = function () {
+var fight = function (enemyName) {
     //alerts player that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = (window.prompt("Would you like to Fight or Skip this battle? Enter 'FIGHT' or 'SKIP' to choose."));
@@ -55,5 +53,12 @@ var fight = function () {
         window.alert("You need ot shoose a valid option. Try again!");
     }
 }
+// game states
+// "win" - player robot has defeated all enemy-robots
+// * Fight all enemy-robots
 
-fight();
+// "lose" - player robot has been defeated
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
