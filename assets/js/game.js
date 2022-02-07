317efd348d28bfe98462b89ec132dceafb4d3dde
Eric Shumway <ericshumway2@mail.weber.edu>
2022-02-06 17:02:06 -0700

    Changes to require proper input for robot's name

--- assets/js/game.js
+++ assets/js/game.js
@@ -117,8 +117,19 @@ var randomNumber = function (min, max) {
 
     return value;
 }
+var getPlayerName = function (){
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name");
+    }
+
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -165,4 +176,5 @@ var enemyInfo = [
         attack: randomNumber(10, 14)
     }
 ];
+
 startGame();
